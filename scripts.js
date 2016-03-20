615b710e8637605b1afc16c3dca5be12954ebdb1
gilau <joyliss_baby_2211@hotmail.com>
2016-03-20 15:55:22 -0500

    enviando pagina con errores

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-console.log("Inicio de programa");
+console,log("Inicio de programa");
 alert("Hola JavaScript");
 document.write("<h1>Hola JavaScript!</h1>");
-alert("gracias por visitar");
-console.log("fin de programa");
+aler("gracias por visitar");
+console.log("fin de programa);

84e3f76b7581a243de4a3c8c3bf26b53714d6576
gilau <joyliss_baby_2211@hotmail.com>
2016-03-20 17:10:06 -0500

    trabajando en consola y html

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
 console.log("Inicio de programa");
-alert("Hola JavaScript");
-document.write("<h1>Hola JavaScript!</h1>");
+/*alert("Hola JavaScript");*/
+/*var nombre= prompt("Cual es tu nombre");
+document.write ("<h1> su nombre es:"  +nombre+"</h1>");
+/*document.write("<h1>Hola JavaScript!</h1>");
 alert("gracias por visitar");
-console.log("fin de programa");
+
+var equipo= prompt("cual es tu equipo favorito?");
+console.log("el equipo es:" +equipo);
+
+var clave = prompt("Hola Usuario, Ingrese su clave en minusculas!");
+console.log("La longitud de la clave es: " + clave.length);
+console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
+console.log("El valor original de la clave es: " +  clave);*/
+
+
+/*console.log("fin de programa");*/
+
+var nombre= prompt("Cual es tu nombre");
+document.write ("<h1> su nombre es:"  +nombre+"</h1>");

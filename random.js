3776d5bf731c16d9a62394733dfe6824067b388e
laura <joyliss_baby_2211@hotmail.com>
2016-03-27 16:03:04 -0500

    agregamos ciclos

--- random.js
+++ random.js
@@ -1,4 +1,6 @@
-/*function tiraDados(){
+
+/******** TIRA DADOS*******
+function tiraDados(){
 	var dado = Math.floor(Math.random() * 6) + 1;
 	return dado;
 } 
@@ -7,20 +9,22 @@ alert("el dado dice: " + tiraDados());
 var multiplo = 2 * tiraDados();
 alert("el multiplo de bonus es: " + multiplo);*/
 
-/*function numeroAlAzarHastaLimite(limite) 
+/******* NUMEROS AL AZAR CON LIMITE ***********
+function numeroAlAzarHastaLimite(limite) 
 {
 	var num = Math.floor(Math.random() * limite) + 1;
 	return num;
 }
 var numAzar = numeroAlAzarHastaLimite(100);
 
-alert("El numero al Azar es: " + numAzar );
-var num1= ("ingrese primer numero");
-var num2 = ("ingrese segundo numero");
+alert("El numero al Azar es: " + numAzar );*/
+
+/*********** MOSTRAR NUMERO MAYOR ENTRE 2 NUMEROS ******
+var num1= parseInt(prompt("ingrese primer numero"));
+var num2 = parseInt(prompt("ingrese segundo numero"));
 
 function max(num1, num2) 
 {
-	var num = Math.floor(Math.random() * max) + 1;
 	if (num1 > num2)
 	{
 	return num1;
@@ -28,10 +32,10 @@ function max(num1, num2)
 	else{
 		return num2;
 	}
-	
-
 }
-*/
+
+alert(max(num1, num2));
+
 /******** variables globales y locales *******
 function saludo(){
 	var mensaje = ("Hola a todos");
@@ -42,6 +46,8 @@ saludo();
 alert(mensaje);
 saludo();*/
 
+
+/******* MENSAJE DE ERROR CUANDO NO ES NUMERICO *****
 var num = 9;
 if(isNaN(num))
 {
@@ -49,4 +55,40 @@ if(isNaN(num))
 }
 else{
 	num = num * 0.5;
+} **********/
+
+
+			/************** DESAFIO  **************
+*******************numero al azar entre 2 limites con errores**************/
+
+function numAzar(lim1, lim2) 
+{
+	if (isNaN(lim1) || isNaN(lim2))
+	{
+	 	throw new Error("Valor ingresado no es numerico");
+	}
+	var num = Math.round(Math.random() * (lim1 - lim2)) + lim2;
+	return num;
+
 }
+var lim1= parseInt(prompt("Ingrese limite superior : "));
+var lim2 = parseInt(prompt("ingrese limite inferior : "));
+/*alert("Su numero al azar es: " + numAzar(parseInt(lim2), parseInt(lim1)));*/
+/***************** agregando ciclos ************/
+
+var contador = 0;
+
+while(contador<10)
+{
+var randomico= numAzar(parseInt(lim2), parseInt(lim1));
+document.write(randomico + " , ");
+contador++;
+}
+
+
+/***************************  CICLOS  ********************************
+
+var lim1= parseInt(prompt("Ingrese limite superior : "));
+var lim2 = parseInt(prompt("ingrese limite inferior : "));*/
+
+
